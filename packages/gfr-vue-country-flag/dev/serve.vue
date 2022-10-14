32331258a41d15c48a4a379f30f5218b1146faa7
davidsilverman <david@dasitconsulting.com>
2022-10-14 16:13:10 +0200

    Added gfr folder

---
+++ packages/gfr-vue-country-flag/dev/serve.vue
@@ -0,0 +1,27 @@
+<script>
+import Vue from 'vue';
+import CountryFlag from '@/CountryFlag.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    CountryFlag
+  },
+  data() {
+    return {
+     items: {value: 'it'}
+    };
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <CountryFlag country='dk' size='big'/>
+    <CountryFlag country='no' size='big'/>
+    <CountryFlag country='se' size='big'/>
+    <CountryFlag country='gb' size='big'/>
+    <CountryFlag country='fr' size='big'/>
+    <CountryFlag country='it' size='big'/>
+  </div>
+</template>
